623e12734fb832d04a62ba7c1e988fcfbed0bc72
khaled-khattab <khlaedadel.fci86@gmail.com>
2019-01-12 11:01:31 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    ResponseResetComponent,
+    RequestResetComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
